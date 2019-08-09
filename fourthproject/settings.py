5ea6ace0e8c9f4c1b0f5a90ac118bc81a4e81815
Donghwa96 <ensk960405@gmail.com>
2019-08-10 06:05:07 +0900

    개발 환경 변경

--- fourthproject/settings.py
+++ fourthproject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = 'h7i527c-1sx4ods1#-sg6q9rd545$7=z(ts_eecz2j2l_a21h+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
